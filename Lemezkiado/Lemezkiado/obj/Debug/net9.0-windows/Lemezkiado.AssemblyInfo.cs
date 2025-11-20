3d1baea4662ad29e696effe7a2efedb14049710f
Pilter Barnabás <pilter.barnabas.jozsef@students.jedlik.eu>
2025-11-20 11:13:46 +0100

    fajlbeolvasas kesz

--- Lemezkiado/Lemezkiado/obj/Debug/net9.0-windows/Lemezkiado.AssemblyInfo.cs
+++ Lemezkiado/Lemezkiado/obj/Debug/net9.0-windows/Lemezkiado.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lemezkiado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8518c243e7a6cb21d138c6c510e0d905f7e355e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6183edfc779370bb86a1cd163905f0633094b8f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lemezkiado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lemezkiado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
